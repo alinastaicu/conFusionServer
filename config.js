95f4531760e20d70eb03344fc90745b37e433a0e
Alina Staicu <alina.staicu@check24.de>
2019-06-10 13:30:21 +0200

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   secretKey: '12345-67890-09876-54321',
   mongoUrl: 'mongodb://localhost:27017/conFusion',
+  facebook: {
+    clientId: '899177823754850',
+    clientSecret: '32777430c2677287f2acfbfba90c9bfc',
+  },
 };
